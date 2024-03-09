bf6e34c4d1e152c987d2123ccffac63870cc494f
Upamanyu <sarangi.upamanyu@gmail.com>
2024-03-09 13:22:55 +0530

    Chore | Remove Boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
+import SiteBuilderContainer from '@/SiteBuilderContainer.vue'
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(SiteBuilderContainer).mount('#app')
